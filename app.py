002ab84407a86855d3152537a1d67c3966b4dfb1
Replit user <>
2023-10-28 17:09:18 +0000

    Adicionei mais letras

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 import random
 
-chars = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 
-'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 
-'z', '-', '+', '_', '#', '$']
+chars = [
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
+    'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+    'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+    'Z'
+    '-', '+', '_', '#', '$'
+]
 
 password = ''
 
+
 for x in range(0, 8):
-    index =  random.randint(0, len (chars) - 1)
-    password += chars[index]
+  index = random.randint(0, len(chars) - 1)
+  password += chars[index]
 
-
-print('A senha gerada é: '+ password)
-
+print('A senha gerada é: ' + password)
